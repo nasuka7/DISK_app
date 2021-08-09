d55e33ed03f54270ff2d3c906a91f04d7f384313
nasuka7 <n4suka7@gmail.com>
2021-08-09 22:55:48 +0900

    ✨ 投稿データをデータベースに保存する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,13 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    Post.create(post_params)
+  end
+  private
+
+  def post_params
+      params.require(:post).permit(:title, :location, :name, :date,)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '', to: 'posts#index'
   
   get 'posts/new', to: 'posts#new'
+
+  post 'posts', to: 'posts#create'
 end
