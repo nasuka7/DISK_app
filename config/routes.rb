2ef194588a457f85e16a5c253c8be3124f0f1ecd
nasuka7 <n4suka7@gmail.com>
2021-08-13 21:14:09 +0900

    🎨 リソースベースルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '', to: 'posts#index'
-  
-  get 'posts/new', to: 'posts#new'
-
-  post 'posts', to: 'posts#create'
-
-  get 'posts/:id', to: 'posts#open'
+  resources :posts, only: [:index, :new, :create, :show]
 end
